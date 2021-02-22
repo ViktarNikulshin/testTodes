063a40866a5c557a55aa762ea5e464fc8f12d8e1
ViktarNikulshin <viktarnikulshin@gmail.com>
2021-02-22 10:29:46 +0300

    edit Description
    ::
    
    ::
    
    ::
    
    :
    main/java/net/pst/TestApplication.java
    
    O

--- src/main/java/net/pst/TestApplication.java
+++ src/main/java/net/pst/TestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.jsf.el.SpringBeanFacesELResolver;
@@ -21,7 +22,7 @@ import java.util.Set;
 
 @SpringBootApplication
 @EnableFeignClients
-public class TestApplication implements ServletContextInitializer {
+public class TestApplication extends SpringBootServletInitializer implements ServletContextInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
